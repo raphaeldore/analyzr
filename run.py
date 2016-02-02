5415eafaa43cf59a05028338c3edd7b057aa070d
Raphaël Doré <rdore@neomalibox.ch>
2016-02-02 16:11:55 -0500

    On check si on est root avant de continuer (pas sur si ça fonctionne sur windows...)

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+from os import getuid
+
 if __name__ == "__main__":
+    # perm check
+    # TODO: Ça fonctionne tu sur windows?
+    if int(getuid()) > 0:
+        print("\033[31m [-] Please run as root. \033[0m")
+        exit(1)
     from analyzr.main import execute
     execute()
