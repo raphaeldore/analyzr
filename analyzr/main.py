78b5ff928ef41dcc4112eecf32298afb41ac0706
Raphaël Doré <rdore@neomalibox.ch>
2016-02-02 15:02:57 -0500

    nouvelle fonction passive_network_scan sniff le réseau pour trouver des hosts qu'il aurait manqué

--- analyzr/main.py
+++ analyzr/main.py
@@ -5,7 +5,8 @@ def execute():
     networkscanner = NetworkScanner()
     networkscanner.scan_and_find_live_hosts_on_networks()
     networkscanner.pretty_print_ips()
-    networkscanner.find_hops()
+    #networkscanner.fingerprints()
+    #networkscanner.find_hops()
 
     #networkscanner.scan_live_hosts_for_opened_ports()
     #networkscanner.port_ping_scan()
--- analyzr/networkscanner.py
+++ analyzr/networkscanner.py
@@ -7,7 +7,7 @@ This modules allows to scan the network of the current host..
 import errno
 from collections import namedtuple
 
-from netaddr import *
+from netaddr import IPNetwork, IPAddress, EUI, NotRegisteredError
 from scapy.all import *
 from scapy.layers.inet import IP, TCP, ICMP, UDP
 from scapy.layers.l2 import arping
@@ -57,9 +57,6 @@ class NetworkScanner:
 
             self.networks_interfaces[interface] = ip_network
 
-    def get_host_network(self):
-        return ""
-
     def port_ping_scan(self):
         try:
             for interface, network in self.networks_interfaces.items():
@@ -80,19 +77,49 @@ class NetworkScanner:
             for interface, network in self.networks_interfaces.items():
                 ans, unans = scapy.layers.l2.arping(str(network), iface=interface, timeout=timeout, verbose=False)
                 for s, r in ans.res:
-                    self.live_network_hosts.setdefault(str(network), []).append(
-                        LiveHost(ip=r[ARP].psrc, mac=r[Ether].src))
+                    self.live_network_hosts.setdefault(network, []).append(
+                        LiveHost(ip=IPAddress(r[ARP].psrc), mac=EUI(r[Ether].src)))
+
+                # On sniff le réseau pendant quelques secondes pour trouver des hôtes additionnels
+                self.passive_network_scan(network)
         except socket.error as e:
             if e.errno == errno.EPERM:  # Operation not permitted
                 logger.error("%s. Did you run as root?", e.strerror)
             else:
                 raise
 
+    def passive_network_scan(self, network):
+        logger.debug("Sniffing network traffic for more hosts.")
+        new_hosts_count = 0
+        ans = sniff(timeout=10)
+        logger.debug("Analyzing traffic.")
+        for i in ans:
+            if IP in i:
+                src = IPAddress(i[IP].src)
+                dst = IPAddress(i[IP].dst)
+            elif ARP in i:
+                src = IPAddress(i[ARP].psrc)
+                dst = IPAddress(i[ARP].pdst)
+            else:
+                continue
+
+            if src in network.cidr and not [live_host for live_host in self.live_network_hosts[network]
+                                            if src == live_host.ip]:
+                new_hosts_count += 1
+                self.live_network_hosts.setdefault(network, []).append(LiveHost(ip=src, mac=EUI(i.src)))
+
+            if dst in network.cidr and not [live_host for live_host in self.live_network_hosts[network]
+                                            if dst == live_host.ip] and i.dst != 'ff:ff:ff:ff:ff:ff':
+                new_hosts_count += 1
+                self.live_network_hosts.setdefault(network, []).append(LiveHost(ip=dst, mac=EUI(i.dst)))
+
+        logger.debug("Passive scan found {0:d} new hosts.".format(new_hosts_count))
+
     def find_hops(self):
         iphops = dict()
         for network, live_hosts in self.live_network_hosts.items():
             for live_host in live_hosts:
-                for hops in range(1, 28): # Max 4 hops..on reste sur le réseau local
+                for hops in range(1, 28):
                     reply = sr1(IP(dst=live_host.ip, ttl=hops) / UDP(dport=40000), verbose=0, timeout=1)
                     if reply is None:
                         # No reply
@@ -107,8 +134,14 @@ class NetworkScanner:
 
     def pretty_print_ips(self):
         for network, live_hosts in self.live_network_hosts.items():
+            print("Live hosts in network {0:s}".format(str(network)))
             for live_host in live_hosts:
-                print("{0:20s} {1:s}".format(live_host.ip, live_host.mac))
+                try:
+                    print("{0:20s} {1:s} --> OUI: {2:s}".format(str(live_host.ip), str(live_host.mac),
+                                                                live_host.mac.oui.registration().org))
+                except NotRegisteredError:
+                    print("{0:20s} {1:s} --> OUI: {2:s}".format(str(live_host.ip), str(live_host.mac),
+                                                                "Unknown OUI"))
 
     def scan_live_hosts_for_opened_ports(self):
         count = sum(len(v) for v in self.live_network_hosts.values())
@@ -116,7 +149,7 @@ class NetworkScanner:
         threads = []
         for network, live_hosts in self.live_network_hosts.items():
             for live_host in live_hosts:
-                t = PortScanThread(portlist=topports, tid=len(threads), target=live_host, results=results)
+                t = PortScanThread(portlist=topports, tid=len(threads), target=str(live_host), results=results)
                 t.start()
                 threads.append(t)
 
