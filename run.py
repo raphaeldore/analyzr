b9b8d9910e54637cd6a160fd2405f19106f73ed1
Raphaël Doré <rdore@neomalibox.ch>
2016-02-08 08:36:53 -0500

    Dans le run.py on regarde si on passe l'argument --force au programme. Si oui on ne check pas si root

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 import sys
+import argparse
 
 from analyzr.utils.admin import isUserAdmin
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--force",
+                        help="Force run the application. Even if not root. Warning : things most probably won't work.",
+                        action='store_true', default=False)
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
 
-    if not __debug__:
+    args = parse_args()
+    if not args.force:
         try:
             # perm check
             if not isUserAdmin():
@@ -15,4 +26,5 @@ if __name__ == "__main__":
             sys.exit(1)
 
     from analyzr.main import execute
+
     execute()
