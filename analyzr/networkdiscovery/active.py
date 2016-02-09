07486edf856de45f9f0af70bb234d2cfa6b5c694
Raphaël Doré <rdore@neomailbox.ch>
2016-02-08 21:48:31 -0500

    Ajout classe ICMPPing. De plus, PortPing devient TCPPing. TCPPing a 2 modes: Fast and Slow. Fast test seulement le port 80, et slow tests toutes les ports définit dans topports.

--- analyzr/networkdiscovery/active.py
+++ analyzr/networkdiscovery/active.py
@@ -1,17 +1,17 @@
+import errno
 import random
 import socket
 
-import errno
 import scapy
 from netaddr import IPAddress, EUI
 from scapy.layers.inet import IP, TCP, ICMP
 from scapy.layers.l2 import ARP, Ether
 from scapy.sendrecv import sr1, sr
 
-from analyzr.topports import topports
 from analyzr.core.entities import NetworkNode
 from analyzr.networkdiscovery.scanner import Scanner
-from analyzr.utils.network import resolve_ip, CommonTCPFlags
+from analyzr.topports import topports
+from analyzr.utils.network import resolve_ip, TCPFlag
 
 
 class ArpPing(Scanner):
@@ -41,12 +41,71 @@ class ArpPing(Scanner):
         self.logger.info("Arp ping scan done. Found %d unique hosts.", self.number_of_hosts_found)
 
 
-class PortPing(Scanner):
+# TODO: This is very slooooow
+class ICMPPing(Scanner):
     def __init__(self):
-        super(PortPing, self).__init__()
+        super(ICMPPing, self).__init__()
 
     def scan(self):
-        self.logger.info("Executing port ping scan...")
+        self.logger.info("Executing ICMP ping scan...")
+        try:
+            for interface, network in self.config["networks_interfaces"]:
+                discovered_hosts = set()
+                ans, unans = sr(IP(dst=str(network)) / ICMP(), iface=interface, timeout = 2)
+                for s, r in ans.res:
+                    node = NetworkNode()
+                    node.ip = IPAddress(r[IP].src)
+                    node.mac = EUI(r[Ether].src)
+                    node.host = resolve_ip(r[Ether].src)
+                    discovered_hosts.add(node)
+
+                self.scan_results[network] = discovered_hosts
+        except socket.error as e:
+            if e.errno == socket.errno.EPERM:  # Operation not permitted
+                self.logger.error("%s. Did you run as root?", e.strerror)
+            else:
+                raise
+
+        self.logger.info("ICMP ping scan done. Found %d unique hosts.", self.number_of_hosts_found)
+
+
+# TODO: This is very slooooow
+class TCPPing(Scanner):
+    def __init__(self):
+        super(TCPPing, self).__init__()
+
+    def scan(self):
+        if self.config["fastTCP"]:
+            self._fastScan()
+        else:
+            self._slowScan()
+
+        self.logger.info("TCP ping scan done. Found %d unique hosts.", self.number_of_hosts_found)
+
+    def _fastScan(self):
+        self.logger.info("Executing TCP ping scan (fast version)...")
+        self.logger.info("Scanning port 80.")
+        try:
+            for interface, network in self.config["networks_interfaces"]:
+                discovered_hosts = set()
+                ans, unans = sr(IP(dst=str(network)) / TCP(dport=80, flags="S"), iface=interface)
+                for s, r in ans.res:
+                    node = NetworkNode()
+                    node.ip = IPAddress(r[IP].src)
+                    node.mac = EUI(r[Ether].src)
+                    node.host = resolve_ip(r[Ether].src)
+                    discovered_hosts.add(node)
+
+                self.scan_results[network] = discovered_hosts
+        except socket.error as e:
+            if e.errno == socket.errno.EPERM:  # Operation not permitted
+                self.logger.error("%s. Did you run as root?", e.strerror)
+            else:
+                raise
+
+    def _slowScan(self):
+        self.logger.info("Executing TCP ping scan (slow version)...")
+        self.logger.info("Scanning ports %s.", str(topports).strip("[]"))
         try:
             for interface, network in self.config["networks_interfaces"]:
                 discovered_hosts = set()
@@ -66,8 +125,6 @@ class PortPing(Scanner):
             else:
                 raise
 
-        self.logger.info("Port ping scan done. Found %d unique hosts", self.number_of_hosts_found)
-
     def _portScan(self, host, ports, interface):
         # Send SYN with random Src Port for each Dst port
         for dstPort in ports:
@@ -75,11 +132,13 @@ class PortPing(Scanner):
             resp = sr1(IP(dst=host) / TCP(sport=srcPort, dport=dstPort, flags="S"), timeout=1, verbose=0,
                        iface=interface)
             if resp is None:
-                # No response... we cannot know if host exists (port is probably filtered, aka silently dropped)
-                pass
+                # No response... we cannot know if host exists (port is probably filtered, aka silently dropped).
+                # Let's try the next port.
+                continue
             elif resp.haslayer(TCP):
-                if resp.getlayer(TCP).flags == CommonTCPFlags.SYN_ACK or resp.getlayer(
-                        TCP).flags == CommonTCPFlags.RST_ACK:
+                if resp.getlayer(TCP).flags == (TCPFlag.SYN | TCPFlag.ACK) or resp.getlayer(
+                        TCP).flags == (TCPFlag.RST | TCPFlag.ACK):
+                    print("| OR WORKS!")
                     send_rst = sr(IP(dst=host) / TCP(sport=srcPort, dport=dstPort, flags="R"), timeout=1, verbose=0,
                                   iface=interface)
                     # We know the port is closed or opened (we got a response), so we deduce that the host exists
@@ -89,8 +148,8 @@ class PortPing(Scanner):
                     node.host = resolve_ip(resp[Ether].src)
                     return NetworkNode
                 elif resp.haslayer(ICMP):
-                    if int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]:
-                        # We cannot know if host exists (port is probably filtered, aka silently dropped)
-                        pass
+                    # We cannot determine if host exists (port is probably filtered, aka silently dropped).
+                    # Let's try the next port.
+                    continue
 
         return None
