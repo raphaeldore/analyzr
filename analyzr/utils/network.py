a8663100493e401585558e0599882b5f680aca4b
Raphaël Doré <rdore@neomailbox.ch>
2016-02-08 21:44:30 -0500

    Ajout enums ICMPType, ICMPCode, TCPFlag

--- analyzr/utils/network.py
+++ analyzr/utils/network.py
@@ -1,6 +1,7 @@
 import math
 import socket
 import struct
+from enum import Enum
 
 
 def long2netmask(arg):
@@ -9,11 +10,11 @@ def long2netmask(arg):
     return 32 - int(round(math.log(0xFFFFFFFF - arg, 2)))
 
 
-def ip2int(addr : bytes):
+def ip2int(addr: bytes):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
 
-def int2ip(addr : int):
+def int2ip(addr: int):
     return socket.inet_ntoa(struct.pack("!I", addr))
 
 
@@ -74,12 +75,68 @@ def resolve_ip(ip):
         host = host[0]
     return host
 
-class CommonTCPFlags:
-    NULL = 0x00
-    SYN = 0x02
-    ACK = 0x10
-    SYN_ACK = 0x12
-    #RST = 0
-    RST_ACK = 0x14
-    FIN = 0x01
-    FIN_ACK = 0x11
+
+class ICMPType(Enum):
+    ECHO_REPLY              = 0
+    DEST_UNREACHABLE        = 3
+    SOURCE_QUENCH           = 4
+    REDIRECT                = 5
+    ECHO_REQUEST            = 8
+    ROUTER_ADVERTISEMENT    = 9
+    ROUTER_SOLICITATION     = 10
+    TIME_EXCEEDED           = 11
+    PARAMETER_PROBLEM       = 12
+    TIMESTAMP_REQUEST       = 13
+    TIMESTAMP_REPLY         = 14
+    INFORMATION_REQUEST     = 15
+    INFORMATION_RESPONSE    = 16
+    ADDRESS_MASK_REQUEST    = 17
+    ADDRESS_MASK_REPLY      = 18
+
+class ICMPCode(Enum):
+    # DEST_UNREACHABLE (3)
+    NETWORK_UNREACHABLE         = 0
+    HOST_UNREACHABLE            = 1
+    PROTOCOL_UNREACHABLE        = 2
+    PORT_UNREACHABLE            = 3
+    FRAGMENTATION_NEEDED        = 4
+    SOURCE_ROUTE_FAILED         = 5
+    NETWORK_UNKNOWN             = 6
+    HOST_UNKNOWN                = 7
+    NETWORK_PROHIBITED          = 9
+    HOST_PROHIBITED            = 10
+    TOS_NETWORK_UNREACHABLE    = 11
+    TOS_HOST_UNREACHABLE       = 12
+    COMMUNICATION_PROHIBITED   = 13
+    HOST_PRECEDENCE_VIOLATION  = 14
+    PRECEDENCE_CUTOFF          = 15
+
+    # REDIRECT (5)
+    NETWORK_REDIRECT        = 0
+    HOST_REDIRECT           = 1
+    TOS_NETWORK_REDIRECT    = 2
+    TOS_HOST_REDIRECT       = 3
+
+    # TIME_EXCEEDED (11)
+    TTL_ZERO_DURING_TRANSIT     = 0
+    TTL_ZERO_DURING_REASSEMBLY  = 1
+
+    # PARAMETER_PROBLEM (12)
+    IP_HEADER_BAD           = 0
+    REQUIRED_OPTION_MISSING = 1
+
+
+class TCPFlag(Enum):
+    NULL    = 0x00
+    FIN     = 0x01
+    SYN     = 0x02
+    RST     = 0x04
+    PSH     = 0x08
+    ACK     = 0x10
+    URG     = 0x20
+
+    def __or__(self, other):
+        return self.value | other.value
+
+
+    # Si on veut un SYN-ACK on fait: SYN_ACK = SYN | ACK
