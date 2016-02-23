7da4714a4978353d6c461eda913aaa8d3c87d788
Raphaël Doré <rdore@neomalibox.ch>
2016-02-23 09:29:00 -0500

    Mise à jour de setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,47 @@
-from distutils.core import setup
+import types
+from os import path as op
+from os.path import join
+from cx_Freeze import setup, Executable
 
-setup(
+from io import StringIO
+
+
+def _read(filename):
+    with open(join(op.dirname(__file__), filename)) as fd:
+        return fd.read()
+
+
+def _read_reqs(filename):
+    is_valid = (lambda _: _
+                          and not any(_.startswith(ch) for ch in ['#', '-']))
+
+    data = getattr(types, 'UnicodeType', str)(_read(filename))
+    return list((_.strip() for _ in StringIO(data) if is_valid(_.strip())))
+
+
+executables = [
+    Executable("run.py")
+]
+
+setup_params = dict(
     name='analyzr',
     version='1.0',
-    packages=['analyzr'],
-    url='',
+    packages=['', 'core', 'utils', 'graphics', 'topology', 'fingerprints', 'networkdiscovery'],
+    package_dir={'': 'analyzr'},
+    url='https://raphaeldore.github.io/analyzr/',
     license='MIT',
     author='Raphaël Doré & Raphaël Fournier',
-    author_email='',
-    description=''
+    author_email='rdore@neomailbox.ch',
+    description='Scannez votre réseau pour découvrir sa topologie!',
+    install_requires=_read_reqs('requirements.txt'),
+    executables=executables
+
 )
+
+
+def main():
+    setup(**setup_params)
+
+
+if '__main__' == __name__:
+    main()
